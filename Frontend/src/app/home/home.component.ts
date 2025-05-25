0b134262c04a0d467fa160ed5a585a0af539d1f2
Ivan <janivantot@gmail.com>
2025-05-25 23:15:22 +0800

    homepage setup

---
+++ Frontend/src/app/home/home.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzCardModule,
+    NzButtonModule,
+    NzDividerModule,
+    NzGridModule,
+  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  features = [
+    {
+      title: 'Enterprise-Class',
+      description:
+        'Designed for enterprise-level applications with a professional UI design language.',
+    },
+    {
+      title: 'Powerful Components',
+      description: 'Rich set of high-quality components out of the box.',
+    },
+    {
+      title: 'TypeScript',
+      description: 'Written in TypeScript with predictable static types.',
+    },
+    {
+      title: 'Themes',
+      description:
+        'Customizable theme with CSS variables for easy style customization.',
+    },
+    {
+      title: 'International',
+      description:
+        'Extensive internationalization support for dozens of languages.',
+    },
+    {
+      title: 'Accessibility',
+      description:
+        'Follows WCAG standards to ensure accessibility for all users.',
+    },
+  ];
+}
